e3a7395f2118a028bca119f17001218d9a568897
Eddie <kta23379745@gmail.com>
2022-07-19 21:54:34 +0800

    feat: simple model setup

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"backend/model"
 	"backend/server"
 )
 
 func main() {
+	model.Setup()
 	server.SetupRouter()
 }
---
+++ model/model.go
@@ -0,0 +1,7 @@
+package model
+
+var RestroomList []Restroom
+
+func Setup() {
+	RestroomList = []Restroom{}
+}
