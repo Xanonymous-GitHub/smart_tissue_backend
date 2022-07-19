2e23c75c26d54a5ea1ac93b42d8f224cf93b6f05
Eddie <kta23379745@gmail.com>
2022-07-19 22:49:22 +0800

    feat: remove test api

--- server/router.go
+++ server/router.go
@@ -2,19 +2,12 @@ package server
 
 import (
 	"backend/server/api"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() {
 	router := gin.Default()
-
-	router.GET("/test", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Your server is working",
-		})
-	})
 
 	router.GET("/restrooms", api.GetAllRestrooms)
 	router.POST("/restroom", api.RegisterRestroom)
