330438b4dc14e964237182c0b85c640892ec6cfd
Eddie <kta23379745@gmail.com>
2022-07-19 22:48:42 +0800

    feat: make get_all_restrooms to real

--- server/api/get_all_restrooms.go
+++ server/api/get_all_restrooms.go
@@ -8,13 +8,9 @@ import (
 )
 
 func GetAllRestrooms(c *gin.Context) {
-	restrooms := []model.Restroom{
-		{Id: "id1", Location: "location1", ToiletIdList: []model.Toilet{}},
-		{Id: "id2", Location: "location2", ToiletIdList: []model.Toilet{}},
-	}
+	restrooms := model.RestroomList
 
 	c.JSON(http.StatusOK, gin.H{
-		"message":   "This is a message just for debugging, remove it when it is unused.",
 		"restrooms": restrooms,
 	})
 }
