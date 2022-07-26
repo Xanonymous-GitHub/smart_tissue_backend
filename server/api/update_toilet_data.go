5be5289135d23e126fe3b8fdda34c405b754418b
Koro <youcry987951@gmail.com>
2022-07-26 17:13:38 +0900

    fix: add return to handler

--- server/api/update_toilet_data.go
+++ server/api/update_toilet_data.go
@@ -18,24 +18,28 @@ func UpdateToiletData(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "Fail to update, please send the id of the target toilet!",
 		})
+		return
 	}
 	jsonPercentage, hasPercentage := json["percentage"]
 	if !hasPercentage {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "Fail to update, please send the new percentage of the toilet!",
 		})
+		return
 	}
 	location, hasLocation := json["location"]
 	if !hasLocation {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "Fail to update, please send the new location of the toilet!",
 		})
+		return
 	}
 	state, hasState := json["state"]
 	if !hasState {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "Fail to update, please send the new state of the toilet!",
 		})
+		return
 	}
 	percentage, _ := strconv.ParseFloat(fmt.Sprint(jsonPercentage), 32)
 	toilet := model.Toilet{Id: fmt.Sprint(id), Percentage: float32(percentage), Location: fmt.Sprint(location), State: model.ToiletState(fmt.Sprint(state))}
@@ -44,7 +48,9 @@ func UpdateToiletData(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "Toilet not exist!",
 		})
+		return
 	}
+
 	model.UpdateToiletData(toilet)
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Update toilet success!",
