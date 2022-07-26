9d98a55543a8195230e5d7381438393a1db29593
Koro <youcry987951@gmail.com>
2022-07-26 17:15:46 +0900

    fix: add return to handler

--- server/api/remove_toilet.go
+++ server/api/remove_toilet.go
@@ -16,36 +16,39 @@ func RemoveToilet(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "Fail to remove, please send the id of the toilet!",
 		})
+		return
 	}
 	restroomId, hasRestroomId := json["restroomId"]
 	if !hasRestroomId {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "Fail to remove, please send the id of the restroom!",
 		})
+		return
 	}
 	isToiletExist := model.IsToiletExists(fmt.Sprint(toiletId))
 	if !isToiletExist {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "Fail to remove, toilet not exist or has not been registered!",
 		})
+		return
 	}
 	isRestroomExist := model.IsRestroomExists(fmt.Sprint(restroomId))
 	if !isRestroomExist {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "Fail to remove, restroom not exist!",
 		})
+		return
 	}
 	isToiletIdInRestroom := model.IsToiletIdInRestroom(fmt.Sprint(toiletId), fmt.Sprint(restroomId))
 	if !isToiletIdInRestroom {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "Fail to remove, toilet is not belong to this restroom!",
 		})
+		return
 	}
 
 	model.RemoveToilet(fmt.Sprint(toiletId), fmt.Sprint(restroomId))
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Delete toilet success!",
 	})
-	
-
 }
