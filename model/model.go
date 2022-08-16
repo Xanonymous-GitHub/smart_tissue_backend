580f68338696a4585605d5a31874795311212f44
Eddie <kta23379745@gmail.com>
2022-08-16 23:58:07 +0800

    refactor: rename RemoveIdFromUndeployedToiletId to RemoveIdFromUndeployedToiletIds

--- model/model.go
+++ model/model.go
@@ -16,7 +16,23 @@ func Setup() {
 	restrooms = map[string]Restroom{}
 	toilets = map[string]Toilet{}
 	undeployedToiletIdList = []string{}
-	nextRestroomId = 1
+	nextRestroomId = 4
+	restrooms["1"] = Restroom{Id: "1", Location: "科研12樓", ToiletIdList: []string{"e8e3-45c2-b9b3", "3597-47c8-91d1", "0fc6-49f6-b55a"}}
+	toilets["e8e3-45c2-b9b3"] = Toilet{Id: "e8e3-45c2-b9b3", Percentage: 20, Distance: 8, MaxDistance: 10, Location: "科研12樓-第1間", State: "sufficient"}
+	toilets["3597-47c8-91d1"] = Toilet{Id: "3597-47c8-91d1", Percentage: 60, Distance: 8, MaxDistance: 20, Location: "科研12樓-第2間", State: "sufficient"}
+	toilets["0fc6-49f6-b55a"] = Toilet{Id: "0fc6-49f6-b55a", Percentage: 5, Distance: 9.5, MaxDistance: 10, Location: "科研12樓-第3間", State: "insufficient"}
+	restrooms["2"] = Restroom{Id: "2", Location: "科研13樓", ToiletIdList: []string{"527f-427b-894e", "8abd-4680-9214", "bbab-4e7b-92ce"}}
+	toilets["527f-427b-894e"] = Toilet{Id: "527f-427b-894e", Percentage: 50, Distance: 10, MaxDistance: 20, Location: "科研13樓-第1間", State: "sufficient"}
+	toilets["8abd-4680-9214"] = Toilet{Id: "8abd-4680-9214", Percentage: 100, Distance: 10, MaxDistance: 10, Location: "科研13樓-第2間", State: "sufficient"}
+	toilets["bbab-4e7b-92ce"] = Toilet{Id: "bbab-4e7b-92ce", Percentage: 0, Distance: 0, MaxDistance: 10, Location: "科研13樓-第3間", State: "insufficient"}
+	restrooms["3"] = Restroom{Id: "3", Location: "科研14樓", ToiletIdList: []string{"5e44-41e4-aa4e", "abc0-4ee0-b2b2", "a693-4a8f-9098"}}
+	toilets["5e44-41e4-aa4e"] = Toilet{Id: "5e44-41e4-aa4e", Percentage: 25, Distance: 7.5, MaxDistance: 10, Location: "科研14樓-第1間", State: "sufficient"}
+	toilets["abc0-4ee0-b2b2"] = Toilet{Id: "abc0-4ee0-b2b2", Percentage: 75, Distance: 2.5, MaxDistance: 10, Location: "科研14樓-第2間", State: "sufficient"}
+	toilets["a693-4a8f-9098"] = Toilet{Id: "a693-4a8f-9098", Percentage: 20, Distance: 16, MaxDistance: 20, Location: "科研14樓-第3間", State: "sufficient"}
+	undeployedToiletIdList = append(undeployedToiletIdList, "2f8d-4ae8-8b8a", "90d3-4b14-919f", "221e-4b68-a054")
+	toilets["2f8d-4ae8-8b8a"] = Toilet{Id: "2f8d-4ae8-8b8a", Percentage: 90, Distance: 1, MaxDistance: 10, State: "sufficient"}
+	toilets["90d3-4b14-919f"] = Toilet{Id: "90d3-4b14-919f", Percentage: 15, Distance: 17, MaxDistance: 20, State: "sufficient"}
+	toilets["221e-4b68-a054"] = Toilet{Id: "221e-4b68-a054", Percentage: 5, Distance: 19, MaxDistance: 20, State: "insufficient"}
 }
 
 func GetAllRestrooms() map[string]Restroom {
@@ -89,7 +105,7 @@ func RegisterToilet(toiletId string, restroomId string, location string) {
 	restroom := restrooms[restroomId]
 	restroom.ToiletIdList = append(restroom.ToiletIdList, toiletId)
 	restrooms[restroomId] = restroom
-	RemoveIdFromUndeployedToiletId(toiletId)
+	RemoveIdFromUndeployedToiletIds(toiletId)
 
 	toilet := toilets[toiletId]
 	toilet.Location = location
@@ -129,7 +145,7 @@ func DeleteRestroom(restroomId string) {
 	delete(restrooms, restroomId)
 }
 
-func RemoveIdFromUndeployedToiletId(toiletId string) {
+func RemoveIdFromUndeployedToiletIds(toiletId string) {
 	for toiletIndex, currentToiletId := range undeployedToiletIdList {
 		if currentToiletId == toiletId {
 			undeployedToiletIdList[toiletIndex] = undeployedToiletIdList[len(undeployedToiletIdList)-1]
